9f19ca5a58950325f06df62f24a43800c1da1a77
Jonas <jonascesconetto@edu.univali.br>
2016-09-04 14:47:00 -0300

    Inicial

---
+++ Trabalho_01_BuscaPadraoWeb/src/trabalho_01_buscapadraoweb/Trabalho_01_BuscaPadraoWeb.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabalho_01_buscapadraoweb;
+
+/**
+ *
+ * @author jc
+ */
+public class Trabalho_01_BuscaPadraoWeb {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
