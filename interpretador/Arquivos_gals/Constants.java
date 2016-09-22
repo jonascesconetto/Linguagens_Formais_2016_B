16ac92a3ffaa2508efef4ef09b8a7a64118b7a8d
Jonas <jonascesconetto@edu.univali.br>
2016-09-21 23:26:37 -0300

    Ultimos Arquivos

---
+++ interpretador/Arquivos_gals/Constants.java
@@ -0,0 +1,16 @@
+package gals;
+
+public interface Constants extends ScannerConstants, ParserConstants
+{
+    int EPSILON  = 0;
+    int DOLLAR   = 1;
+
+    int t_MAIS = 2;
+    int t_MENOS = 3;
+    int t_VEZES = 4;
+    int t_DIVIDE = 5;
+    int t_ABRE_P = 6;
+    int t_FECHA_P = 7;
+    int t_INTEIRO = 8;
+
+}
